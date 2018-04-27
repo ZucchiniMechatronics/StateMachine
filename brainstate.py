6ba429b90828979288c774dedbe4f7a309226a87
Bdawgdeep <36241121+Bdawgdeep@users.noreply.github.com>
2018-04-27 03:36:07 +0000

    Create brainstate.py

---
+++ brainstate.py
@@ -0,0 +1,52 @@
+
+#import the correct files and registers
+
+
+
+#generate coordinate system using MAPPING/node_generator.py
+
+
+
+#check the gps coordinates using RFM69/parsedGPS.py
+
+
+
+#find closest node information using node finder
+
+
+
+#use pathfinder algorithm for proper driver location Dijkstra
+
+
+#Send drive instructions to arduino
+
+
+
+#once final node before passenger is achieved run algorithm for Arduino to drive until passenger is found
+
+
+
+#pick up passenger using Lasso commands
+
+
+#drive to closest node
+
+
+#dijkstra to find shortest path
+
+
+#send drive instructions to arudino
+
+
+#once final node closes to destination is reached. drive and continuous check gps until range value is reached
+
+
+#drop off passenger using lasso commands
+
+
+#check RFM for new coordinates.  repeat all
+
+
+
+
+
