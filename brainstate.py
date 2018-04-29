44d3b16e3a6e3904858f3d53ee764ef769b77394
Bdawgdeep <36241121+Bdawgdeep@users.noreply.github.com>
2018-04-29 11:27:16 -0600

    Update brainstate.py

--- brainstate.py
+++ brainstate.py
@@ -5,12 +5,12 @@
 
 #generate coordinate system using MAPPING/node_generator.py
 #node_generator.py  COMMAND FUNCTION: node_generator(x,y) INPUTS: a1 locations measured by camera
-
+print('Phase 1 MAPPING')
 MAP=node_generator
 
 #check the gps coordinates using RFM69/parsedGPS.py
 #parsedGPS.py COMMAND FUNCTION: gpsgo()
-
+print(Phase 2 GPS')
 [ROB,PSG,DES]=gpsgo()
 
 
@@ -20,17 +20,20 @@ MAP=node_generator
 
 #use pathfinder algorithm for proper driver location Dijkstra
 #dijkstra.py COMMAND FUNCTION: pathdata("start","end") INPUTS: ex. pathdata("a1","d2")
-
+print('Phase 3 Path finding')
 PATH=pathdata(start,end)
 
 #asses drive instructions per node. Continuous loop to send node case until next node is in range. Then send next node drive instructions. 
 #connections.py  COMMAND FUNCTION: connections(directions)
-
+print('Phase 4 connections')
 CONNECT=connections(PATH)
+
+print('Phase 5
+i=0
   while (i<(len(PATH)-1)):
     PING=gpsgo()
     ROB=PING[1] 
-    wh
+    
     #calculates drive instructions
     drive_instruct=instruction(CONNECT[i])
     #Send drive instructions to arduino using i2c communications
